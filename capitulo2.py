1d104424bd624c3ac2cb7725f787e809edc48caf
saelric <saelric@gmail.com>
2017-10-23 09:54:31 -0500

    capitulo 2

---
+++ capitulo2.py
@@ -0,0 +1,64 @@
+#Tipo de variable
+x= 35
+type(z)
+
+#Enteros, reales y complejos
+a=35
+b=4.5
+c=3.2 + 7j
+
+# Operadores
+# + suma
+# - resta
+# * multiplicacion
+# % resto
+# / div ision real
+# // division entera
+# ** potencia
+# | or(bit)
+# ^ xor
+# & and
+# == igualdad
+# != desigualdad
+# or or(logico)
+# and and(logico)
+# not negacion
+
+#Funciones Matematicas
+
+#Valor absoluto
+abs(-47,67)
+
+#raiz cuadrada
+import math
+math.sqrt(169)
+
+#Conversion hexadecimal
+hex(16)
+
+#Conversion Octal
+oct(8)
+
+#Conversion Binario
+bin(0xfe)
+
+#Conjuntos
+
+conjunto=set('846')
+
+#add() and remove()
+
+#Principales funciones y metodos
+cad="Cadena de texto de ejemplo"
+
+#longitud de la cadena
+len(cad)
+
+#devuelve el indice correspondiente al elemento buscado
+cad.find(c)
+
+#Remplazar subcadenas
+cad.replace("Hola","Adios")
+
+
+
